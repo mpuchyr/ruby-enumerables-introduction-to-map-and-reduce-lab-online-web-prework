ef451e36a0df22ba5a1aa659930479ab2187ce88
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-13 22:28:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -22,3 +22,13 @@ def map_to_double(source_array)
   end
   new_array
 end
+
+def map_to_square(source_array)
+  counter = 0
+  new_array = []
+  while counter < source_array.length do
+    new_array.push(source_array[counter] ** 2)
+    counter += 1
+  end
+  new_array
+end
