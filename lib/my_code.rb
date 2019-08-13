0d4d2d00fb5a6068ccc49dbfd0adc261f75a75bc
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-13 22:30:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,4 +32,12 @@ def map_to_square(source_array)
   new_array
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
+  total = starting_point
+  counter = 0
+  while counter < source_array.length do
+    total += sourace_array[counter]
+    counter += 1
+  end
+  total
+end
