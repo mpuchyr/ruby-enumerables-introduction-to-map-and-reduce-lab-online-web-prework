1dc3c4971e98342d90b43d0c5bdd5b9acb233029
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-13 22:26:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,3 +12,13 @@ end
 def map_to_no_change(source_array)
   source_array
 end
+
+def map_to_double(source_array)
+  counter = 0
+  new_array = []
+  while counter < source_array.length do
+    new_array.push(source_array[counter] * 2)
+    counter += 1
+  end
+  new_array
+end
